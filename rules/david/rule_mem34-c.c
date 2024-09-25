3e7c7f302a289dd1d5ec8ef86e79e368bc58d25c
stattek <alienhuntsgames@gmail.com>
2024-09-24 23:25:39 -0500

    Add two more rules.

---
+++ rules/david/rule_mem34-c.c
@@ -0,0 +1,32 @@
+/**
+ * Author: David Slay
+ * Summary: Rule MEM34-C example working code
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MY_STRING_LENGTH 25
+
+int main()
+{
+    char *myHeapStr = NULL;
+
+    // allocate memory on the heap
+    myHeapStr = malloc(MY_STRING_LENGTH);
+    if (myHeapStr == NULL)
+    {
+        printf("String not allocated by malloc.");
+        // exit so we don't possibly try to free memory that wasn't dynamically allocated
+        return 1;
+    }
+
+    // get user input string
+    fgets(myHeapStr, MY_STRING_LENGTH, stdin);
+
+    printf("My string on the heap: %s\n", myHeapStr);
+    // only freeing the memory that was dynamically allocated
+    free(myHeapStr);
+    
+    return 0;
+}
---
+++ rules/david/rule_str38-c.c
@@ -0,0 +1,34 @@
+/**
+ * Author: David Slay
+ * Summary: Rule STR38-C example working code
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // string functions
+#include <wchar.h>  // wide string functions
+
+#define MY_STRING_LENGTH 25
+
+int main()
+{
+    wchar_t wideCharStrSrc[MY_STRING_LENGTH + 1] = L"This is a wide string";
+    wchar_t wideCharStrDst[MY_STRING_LENGTH + 1] = L"";
+
+    // use wide string copy function
+    wcsncpy(wideCharStrDst, wideCharStrSrc, MY_STRING_LENGTH);
+
+    char charStrSrc[MY_STRING_LENGTH + 1] = "This is a string";
+    char charStrDst[MY_STRING_LENGTH + 1] = "";
+
+    // use normal string copy function
+    strncpy(charStrDst, charStrSrc, MY_STRING_LENGTH);
+
+    // get length of strings (using correct functions) before null-terminating character
+    size_t wideCharLen = wcslen(wideCharStrDst);
+    size_t charLen = strlen(charStrDst);
+
+    // print out their lengths
+    printf("Length of the wide string: %zu\nLength of normal string: %zu\n", wideCharLen, charLen);
+
+    return 0;
+}
