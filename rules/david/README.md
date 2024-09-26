c7e19a3eed7c66346d85fc02d9c344d79b7be393
stattek <alienhuntsgames@gmail.com>
2024-09-25 21:47:37 -0500

    Implement rule FLP32. - Add README for information on compilation.

---
+++ rules/david/README.md
@@ -0,0 +1,5 @@
+# README
+
+## Compiling
+- For most of these examples, `gcc fileName.c` will work for compiling, with these exceptions:
+    - `rule_flp32.c`: Can be compiled with `gcc rule_flp32.c -lm`.
---
+++ rules/david/rule_flp32.c
@@ -0,0 +1,52 @@
+/**
+ * Author: David Slay
+ * Summary: Rule FLP32-C example working code
+ */
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+/**
+ * @brief Safely does asin(), checking to see if it can be done.
+ *
+ * @param num The number to do the asin() on.
+ * @param resultOut The output result.
+ *
+ * @returns true on error, false on success.
+ */
+bool safe_asin(double num, double *resultOut)
+{
+    if (!resultOut || !isgreaterequal(num, -1.0) || !islessequal(num, 1.0))
+    {
+        // outside of desired range of (-1 <= x <= 1) or resultOut is NULL
+        return true;
+    }
+
+    // safely do this math now
+    *resultOut = asin(num);
+    return false;
+}
+
+int main()
+{
+    double result = 0;
+    double num = 0;
+
+    printf("Input number to perform asin on: ");
+
+    if (scanf("%lf", &num) <= 0)
+    {
+        fprintf(stderr, "Could not read user input\n");
+        return 1;
+    }
+
+    if (safe_asin(num, &result))
+    {
+        fprintf(stderr, "Could not perform safe_asin (num: %lf result: %lf)\n", num, result);
+        return 1;
+    }
+
+    printf("Result: %lf\n", result);
+
+    return 0;
+}
