2f0f7390f5eabca5cb0a5ea30edb748d6bcfa8ed
stattek <alienhuntsgames@gmail.com>
2024-10-01 21:52:23 -0500

    Implement recommendation STR02.

---
+++ recommendations/david/rec_str02.c
@@ -0,0 +1,58 @@
+/**
+ * Author: David Slay
+ * Summary: Rule STR02-C example working code
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+
+#define DIRECTORY_STRING_LENGTH 1024
+
+bool isValidString(char *targetStr, const char *validCharacters)
+{
+    bool isValid = false;
+
+    char *lastCharPtr = targetStr + strlen(targetStr) - 1; // find the last character of the string
+    if (lastCharPtr && *lastCharPtr == '\n')
+    {
+        *lastCharPtr = '\0'; // make this the new end of the string if it's a newline char
+    }
+
+    // find the length of the string until we find a character that is invalid
+    size_t numValidChars = strspn(targetStr, validCharacters);
+    if (numValidChars == strlen(targetStr))
+    {
+        // if our input string is all correct characters then it is sanitized
+        isValid = true;
+    }
+
+    return isValid;
+}
+
+int main(void)
+{
+    static const char VALID_CHARACTERS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/._";
+
+    char directoryStr[DIRECTORY_STRING_LENGTH];
+    memset(directoryStr, 0, DIRECTORY_STRING_LENGTH);
+    char *readStrPtr;
+
+    bool nameIsSanitized = false;
+    do
+    {
+        printf("Enter a directory to use ls on: ");
+        // read from user
+        readStrPtr = fgets(directoryStr, DIRECTORY_STRING_LENGTH, stdin);
+
+        // check that string is sanitized
+        nameIsSanitized = isValidString(readStrPtr, VALID_CHARACTERS);
+    } while (!nameIsSanitized);
+
+    printf("\n\nOutput of ls:\n");
+
+    // execute ls on the directory specified
+    execlp("/bin/ls", "ls", directoryStr, (char *)0);
+    return 0;
+}
