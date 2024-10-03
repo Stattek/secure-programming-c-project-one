70834fe880ac307106278552eea39fa5e97ea964
Losongz <losongz16@gmail.com>
2024-10-02 20:35:27 -0500

    Create STR00.c

---
+++ recommendations/yourName/STR00.c
@@ -0,0 +1,34 @@
+/*
+Description: Recommendation STR00-C states that you should represent characters using an appropriate type. In essence this means that depending on how you use 
+    characters you should use specific types of characters when initializing the entity to hold those characters.
+
+Why it is important: This recommendation is important because if it is followed it can eliminate a large quantity of common programming errors many of which lead to
+    different vulnerabilities in the system that they are are a part of.
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    FILE *file_pointer;
+    int character;
+    char file_name[100];
+
+    printf("Enter file name:\n");
+    scanf("%99s",file_name);
+
+    file_pointer = fopen(file_name, "r"); 
+
+    if (file_pointer == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    while ((character = fgetc(file_pointer)) != EOF) {
+        printf("%c", character);
+    }
+
+    fclose(file_pointer);
+    return 0;
+}
