cd49c529c2c6b153bc042e9a955b4e859524de8e
Losongz <losongz16@gmail.com>
2024-10-13 16:03:30 -0500

    Create rec_pre11.c

---
+++ recommendations/colton/rec_pre11.c
@@ -0,0 +1,26 @@
+/**
+ * Author: Colton Longstreth
+ * Summary: Recommendation PRE11-C example working code
+ */
+
+#include <stdio.h>
+
+#define power_of_two(power) {sum = 1; for(i = 0; i < power; i++) {sum = sum*2;}}
+
+int i;
+int sum;
+
+int main(void){
+    int power_of_two_wanted;
+    //takes user input for wanted power of 2
+    printf("Enter power of 2 desired: ");
+    scanf("%d",&power_of_two_wanted);
+
+    //uses macro to calculate power of 2
+    power_of_two(power_of_two_wanted);
+    //prints value
+    printf("Value: %d",sum);
+
+    return 0;
+}
+
