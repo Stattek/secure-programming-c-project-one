aedf7bb77753b1d67b7145325bbf177184985169
Stattek <alienhuntsgames@gmail.com>
2024-10-13 16:43:41 -0500

    Fix formatting of recommendations.

--- recommendations/colton/rec_pre11.c
+++ recommendations/colton/rec_pre11.c
@@ -5,22 +5,29 @@
 
 #include <stdio.h>
 
-#define power_of_two(power) {sum = 1; for(i = 0; i < power; i++) {sum = sum*2;}}
+#define power_of_two(power)         \
+    {                               \
+        sum = 1;                    \
+        for (i = 0; i < power; i++) \
+        {                           \
+            sum = sum * 2;          \
+        }                           \
+    }
 
 int i;
 int sum;
 
-int main(void){
+int main(void)
+{
     int power_of_two_wanted;
-    //takes user input for wanted power of 2
+    // takes user input for wanted power of 2
     printf("Enter power of 2 desired: ");
-    scanf("%d",&power_of_two_wanted);
+    scanf("%d", &power_of_two_wanted);
 
-    //uses macro to calculate power of 2
+    // uses macro to calculate power of 2
     power_of_two(power_of_two_wanted);
-    //prints value
-    printf("Value: %d",sum);
+    // prints value
+    printf("Value: %d\n", sum);
 
     return 0;
 }
-
